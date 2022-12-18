2d4fec3b00f9d3d38e743534112cce0abf9e7903
Umut Hökelek <umuthokelek@riseup.net>
2022-12-19 01:36:03 +0300

    Create MaxHeap pop method and other related methods

--- Program.cs
+++ Program.cs
@@ -65,10 +65,13 @@ updateDate(parkIsimInput,parkTarihInput);
 printMilliParkHashTable(parkTable);
 
 
-
 CustomMaxHeap maxHeap = new CustomMaxHeap(pq.Count);
 while (pq.TryDequeue(out MilliPark item, out int priority))
 {
     maxHeap.insert(item);
 }
-maxHeap.printHeap();
+
+Console.WriteLine("Yuzolcumune gore en buyuk olup MaxHeap'ten cikarilan parklarin bilgileri: ");
+Console.WriteLine(maxHeap.pop());
+Console.WriteLine(maxHeap.pop());
+Console.WriteLine(maxHeap.pop());
